7888dfbcea77cd888e42d12cacd66a5bbcddf0a0
HeathenUK <65458864+HeathenUK@users.noreply.github.com>
2022-08-06 20:15:09 +0100

    Update ESPAT.h

--- ESPAT.h
+++ ESPAT.h
@@ -15,21 +15,19 @@ typedef struct {
     uint8_t     data[1024];
 } RingBuffer;
 
-// typedef enum {
-//     TCP,
-//     I2C
-// } AT_TYPE;
+typedef enum {
+    I2C_READ,
+    TCP_RECEIVE
+} ESP_QUERY_TYPE;
 
-typedef struct {
-    char        type[6];
-    uint8_t     data_len;
-    uint16_t    data_ptr;
-    uint8_t     data[1024];
-} AT_packet;
+typedef enum {
+    NO_QUERY,
+    QUERY_RAISED,
+    QUERY_READY
+} ESP_QUERY_STATE;
 
 typedef enum {
     STATE_DISCARD,
-    STATE_AWAIT_PLUS,
     STATE_AWAIT_D,
     STATE_AWAIT_R,
     STATE_AWAIT_V,
@@ -43,10 +41,20 @@ typedef enum {
 } STATE_ESP;
 
 typedef struct {
-    STATE_ESP       state;
-    uint8_t     packet[2048];          // I don't know how big this needs to be - size of biggest packet
-    uint16_t    packet_ptr;            // If packet array gets bigger, this needs more bits too...
-    uint16_t    remain_len;            // Fill length remaining (only valid in STATE_FILL_DATA)
+    ESP_QUERY_TYPE      type;
+    ESP_QUERY_STATE     status;
+    STATE_ESP           state;
+    uint8_t             data_len;
+    uint16_t            data_ptr;
+    char*               preamble;
+    uint8_t             query_data[64];
+} ESP_Query;
+
+typedef struct {
+    STATE_ESP      state;
+    // uint8_t     packet[2048];          // I don't know how big this needs to be - size of biggest packet
+    // uint16_t    packet_ptr;            // If packet array gets bigger, this needs more bits too...
+    // uint16_t    remain_len;            // Fill length remaining (only valid in STATE_FILL_DATA)
     RingBuffer  *ringBuffer;           // Ringbuffer in use 
 } State_esp;
 
