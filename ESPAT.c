15e4c9f543597693b12819a8f228f6dbb71e18b5
HeathenUK <65458864+HeathenUK@users.noreply.github.com>
2022-08-06 15:21:34 +0100

    Update ESPAT.c

--- ESPAT.c
+++ ESPAT.c
@@ -6,7 +6,7 @@
 #include <malloc.h>
 #include "ESPAT.h"
 
-#define STANDARD_NOP 700
+#define STANDARD_NOP 1000
 
 void nop_loop(uint16_t n) {
 
@@ -20,6 +20,8 @@ bool queryraised;
 AT_packet at;
 
 extern uint16_t unbuffer(RingBuffer *rb, unsigned char *buffer);
+
+static uint8_t last_c = 0x00;
 
 uint16_t process_incoming_esp(State_esp *state) {
     
@@ -42,27 +44,14 @@ uint16_t process_incoming_esp(State_esp *state) {
         for (int i = 0; i < count; i++) {
             
             if (queryraised == true) {
-                fctprintf(mcSendDevice, &duart_a, "Processing following a query being raised\r\n");
+                fctprintf(mcSendDevice, &duart_a, "Processing following a query being raised, prior c was %c (0X%02x)\r\n", last_c, last_c);
 
                 switch(state->state) {
                     
                     case STATE_DISCARD:
-                        if (buffer[i] == '\r') {
-                            state->state = STATE_AWAIT_PLUS;
-                            fctprintf(mcSendDevice, &duart_a, "Start of a \\r, be on the lookout for a +\r\n");
-                            printf("%c", buffer[i]);
-                        } else {
-                            state->state = STATE_DISCARD;
-                            printf("%c", buffer[i]);
-                        }
-                        break;
-                    
-                    
-                    case STATE_AWAIT_PLUS:
-                        if (buffer[i] == '+') {
-                                state->state = STATE_AWAIT_D;
-                                fctprintf(mcSendDevice, &duart_a, "Got a D!\r\n");
-                                printf("%c", buffer[i]);
+                        if (buffer[i] == '+' && last_c != 'T') {
+                            state->state = STATE_AWAIT_D;
+                            fctprintf(mcSendDevice, &duart_a, "Non-AT + received\r\n");
                         } else {
                             state->state = STATE_DISCARD;
                             printf("%c", buffer[i]);
@@ -73,7 +62,6 @@ uint16_t process_incoming_esp(State_esp *state) {
                         if (buffer[i] == 'D') {
                                 state->state = STATE_AWAIT_R;
                                 fctprintf(mcSendDevice, &duart_a, "Got a D!\r\n");
-                                printf("%c", buffer[i]);
                         } else {
                             state->state = STATE_DISCARD;
                             printf("%c", buffer[i]);
@@ -153,6 +141,7 @@ uint16_t process_incoming_esp(State_esp *state) {
 
                     case STATE_AWAIT_COLON:
                         if (buffer[i] == ':') {
+                                at.data_ptr = 0;
                                 state->state = STATE_AWAIT_DATA;
                                 fctprintf(mcSendDevice, &duart_a, "Got a colon!\r\n");
                         } else {
@@ -174,7 +163,8 @@ uint16_t process_incoming_esp(State_esp *state) {
             }
 
             else printf("%c", buffer[i]);
-             
+        
+        last_c = buffer[i];
         }
 
         return count;
@@ -182,12 +172,14 @@ uint16_t process_incoming_esp(State_esp *state) {
 
 void i2c_read(CharDevice *uart, unsigned int i2caddr, uint8_t reg, uint8_t len) {
     
-    
-    fctprintf(mcSendDevice, uart, "AT+DRVI2CWRBYTES=0,0x%02x,0x%02x\r", i2caddr, reg);
+    i2c_write(uart, i2caddr, reg);
+    //fctprintf(mcSendDevice, uart, "AT+DRVI2CWRBYTES=0,0x%02x,1,0x%02x\r", i2caddr, reg);
     nop_loop(STANDARD_NOP);
     //if (mcGetDevice(0, &duart_a)) fctprintf(mcSendDevice, &duart_a, "AT+DRVI2CWRBYTES=0,0x%02X,2,0x%02X40\r\n", i2caddr, cmd);
     at.data_len = len;
+    nop_loop(STANDARD_NOP);
     fctprintf(mcSendDevice, uart, "AT+DRVI2CRD=0,0x%02x,%d\r", i2caddr, len);
+    printf("\n");
     queryraised = true;
     nop_loop(STANDARD_NOP);
 
@@ -196,6 +188,7 @@ void i2c_read(CharDevice *uart, unsigned int i2caddr, uint8_t reg, uint8_t len) 
 void i2c_write(CharDevice *uart, unsigned int i2caddr, uint8_t cmd) {
     
     fctprintf(mcSendDevice, uart, "AT+DRVI2CWRBYTES=0,0x%02x,1,0x%02X\r", i2caddr, cmd);
+    printf("\n");
     //if (mcGetDevice(0, &duart_a)) fctprintf(mcSendDevice, &duart_a, "AT+DRVI2CWRBYTES=0,0x%02X,2,0x%02X40\r\n", i2caddr, cmd);
     nop_loop(STANDARD_NOP);
 
@@ -204,6 +197,7 @@ void i2c_write(CharDevice *uart, unsigned int i2caddr, uint8_t cmd) {
 
 void i2c_write_2(CharDevice *uart, unsigned int i2caddr, uint8_t cmd2, uint8_t cmd) {
     fctprintf(mcSendDevice, uart, "AT+DRVI2CWRBYTES=0,0x%02x,2,0x%02X%02X\r", i2caddr, cmd, cmd2);
+    printf("\n");
     //if (mcGetDevice(0, &duart_a)) fctprintf(mcSendDevice, &duart_a, "AT+DRVI2CWRBYTES=0,0x%02X,2,0x%02X40\r\n", i2caddr, cmd);
     nop_loop(STANDARD_NOP);
 
@@ -211,6 +205,7 @@ void i2c_write_2(CharDevice *uart, unsigned int i2caddr, uint8_t cmd2, uint8_t c
 
 void i2c_write_3(CharDevice *uart, unsigned int i2caddr, uint8_t cmd3, uint8_t cmd2, uint8_t cmd) {
     fctprintf(mcSendDevice, uart, "AT+DRVI2CWRBYTES=0,0x%02x,3,0x%02X%02X%02X\r", i2caddr, cmd, cmd2, cmd3);
+    printf("\n");
     //if (mcGetDevice(0, &duart_a)) fctprintf(mcSendDevice, &duart_a, "AT+DRVI2CWRBYTES=0,0x%02X,3,0x%02X%02XC0\r\n", i2caddr, cmd, cmd2);
     nop_loop(STANDARD_NOP);
 
@@ -218,6 +213,7 @@ void i2c_write_3(CharDevice *uart, unsigned int i2caddr, uint8_t cmd3, uint8_t c
 
 void i2c_write_4(CharDevice *uart, unsigned int i2caddr, uint8_t cmd4, uint8_t cmd3, uint8_t cmd2, uint8_t cmd) {
     fctprintf(mcSendDevice, uart, "AT+DRVI2CWRBYTES=0,0x%02x,4,0x%02X%02X%02X%02X\r", i2caddr, cmd, cmd2, cmd3, cmd4);
+    printf("\n");
     // if (mcGetDevice(0, &duart_a)) fctprintf(mcSendDevice, &duart_a, "AT+DRVI2CWRBYTES=0,0x%02X,4,0x%02X%02X%02X40\r\n", i2caddr, cmd, cmd2, cmd3);
     nop_loop(STANDARD_NOP);
 
@@ -226,5 +222,6 @@ void i2c_write_4(CharDevice *uart, unsigned int i2caddr, uint8_t cmd4, uint8_t c
 void i2c_setup(CharDevice *uart, uint8_t channel, uint8_t scl, uint8_t sda, int speed) {
 
     fctprintf(mcSendDevice, uart, "AT+DRVI2CINIT=%u,%u,%u,%d\r", channel, scl, sda, speed);
+    printf("\n");
 
 }
